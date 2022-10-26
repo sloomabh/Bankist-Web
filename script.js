d77a1e9fde3347f84873165fb24d8c3768c29d2d
salimbh <sloomabbenhalima95@gmail.com>
2022-10-26 18:07:28 +0900

    add scroll btn

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  ///Scrolling
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+h1.addEventListener('mouseenter', alertH1);
+h1.removeEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
